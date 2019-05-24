4b889c07fd5cdfab4fb44878d05301a723b41dfd
Philipp Kewisch <mozilla@kewis.ch>
2019-05-24 08:04:50 +0200

    Centralize configuration functions

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,11 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  * Portions Copyright (C) Philipp Kewisch, 2019 */
 
+import fs from "fs";
 import os from "os";
+import path from "path";
+
+var gConfigData = null;
 
 export function swap(obj) {
   return Object.keys(obj).reduce((swapped, key) => {
@@ -32,5 +36,48 @@ export function requiresVPN() {
 
   if (!hasVPN) {
     throw new Error("This call requires VPN, looks like you are not connected");
+  }
+}
+
+/**
+ * Read the configuration file, which can either still be an ini file, or a JSON file.
+ *
+ * @return {Object}         The configuration object.
+ */
+function readConfig() {
+  let data;
+  try {
+    data = fs.readFileSync(path.join(os.homedir(), ".amorc"), "utf-8");
+  } catch (e) {
+    return {};
+  }
+
+  if (data[0] == "[") {
+    throw new Error("Your ~/.amorc is still in ini format, you need to convert it to JSON");
+  }
+
+  return JSON.parse(data);
+}
+
+/**
+ * Return the configuration data from the file, either by reading it or the cached copy.
+ *
+ * @param {...string} configpath    The configuration path to look up.
+ * @return {Object}                 The configuration object at this path.
+ */
+export function getConfig(...configpath) {
+  if (!gConfigData) {
+    gConfigData = readConfig();
+  }
+
+  if (configpath) {
+    let data = gConfigData;
+    while (data && configpath.length) {
+      let next = configpath.shift();
+      data = data[next];
+    }
+    return data;
+  } else {
+    return gConfigData;
   }
 }
