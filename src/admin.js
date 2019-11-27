c3f4bb96621f1f87f96f30c04c3a1963862c13f4
Philipp Kewisch <mozilla@kewis.ch>
2019-11-27 14:55:33 +0100

    Preserve files order in admin page forms

--- src/admin.js
+++ src/admin.js
@@ -48,8 +48,8 @@ export class AddonAdminPage {
     this.addonId = addonId;
     this.autocommit = true;
     this._addonSlug = null;
-    this.files = [];
     this.loaded = false;
+    this.pages = [];
   }
 
   get addonSlug() {
@@ -64,18 +64,20 @@ export class AddonAdminPage {
 
   async load() {
     this.loaded = true;
-    this.files = await this.loadPage(1);
+
+    let page1 = await this.loadPage(1);
+    this.pages = [page1];
+
     let promises = [];
     for (let i = 2; i <= this.pageCount; i++) {
       promises.push(this.loadPage(i));
     }
 
-    let pages = await Promise.all(promises);
-    for (let files of pages) {
-      this.files.push(...files);
-    }
+    this.pages.push(...await Promise.all(promises));
+  }
 
-    return this.files;
+  get files() {
+    return this.pages.reduce((allFiles, pageFiles) => allFiles.concat(pageFiles), []);
   }
 
   async loadPage(page=1) {
@@ -100,7 +102,10 @@ export class AddonAdminPage {
       let continuationRow = isContinuation ? row.previousElementSibling : row;
       let rowBase = isContinuation ? -2 : 0;
 
-      files.push({
+      let formIdElement = row.querySelector(`td:nth-of-type(${rowBase + 6}) select`);
+      let formId = formIdElement.getAttribute("name").split("-")[1];
+
+      files[formId] = {
         id: parseInt(row.querySelector(`td:nth-of-type(${rowBase + 4}) a`).textContent, 10),
         name: row.querySelector(`td:nth-of-type(${rowBase + 4}) a`).getAttribute("title"),
         date: continuationRow.querySelector("td:nth-of-type(1)").textContent,
@@ -112,7 +117,7 @@ export class AddonAdminPage {
         platforms: row.querySelector(`td:nth-of-type(${rowBase + 5})`).textContent,
         status: this.getFileStatus(row, rowBase),
         hash: row.querySelector(`td:nth-of-type(${rowBase + 7}) a`).getAttribute("title")
-      });
+      };
     }
 
     return files;
@@ -120,54 +125,68 @@ export class AddonAdminPage {
 
   getFileStatus(row, rowBase) {
     let column = row.querySelector(`td:nth-of-type(${rowBase + 6})`);
-    if (column.innerText === "Deleted") {
+    let text = [...column.childNodes].filter(node => node.nodeType == 3).map(node => node.nodeValue.trim()).join("");
+    if (text == "Deleted") {
       return ADDON_STATUS.DELETED;
     } else {
       return parseInt(column.querySelector("select").value, 10);
     }
   }
 
-  async update(files=null) {
-    if (files === null) {
-      files = this.files;
-    }
+  async update() {
+    let forms = this.pages.map(files => {
+      let form = {
+        "status": this.status,
+        "csrfmiddlewaretoken": this.token,
+        "form-TOTAL_FORMS": files.length,
+        "form-INITIAL_FORMS": files.length,
+        "form-MIN_NUM_FORMS": 0,
+        "form-MAX_NUM_FORMS": Math.max(1000, files.length),
+      };
 
-    let form = {
-      "status": this.status,
-      "csrfmiddlewaretoken": this.token,
-      "form-TOTAL_FORMS": files.length,
-      "form-INITIAL_FORMS": files.length,
-      "form-MIN_NUM_FORMS": 0,
-      "form-MAX_NUM_FORMS": Math.max(1000, files.length),
-    };
+      for (let i = 0, len = files.length; i < len; i++) {
+        form[`form-${i}-id`] = files[i].id;
 
-    for (let i = 0, len = files.length; i < len; i++) {
-      form[`form-${i}-status`] = files[i].status;
-      form[`form-${i}-id`] = files[i].id;
-    }
+        if (files[i].status == ADDON_STATUS.DELETED) {
+          form[`form-${i}-status`] = ADDON_STATUS.DISABLED;
+        } else {
+          form[`form-${i}-status`] = files[i].status;
+        }
+      }
 
-    let uri = `${AMO_ADMIN_BASE}/addon/manage/${this.addonSlug}/`;
-    let response = await this.session.request({
-      method: "POST",
-      uri: uri,
-      form: form,
-      headers: { Referer: uri }
+      return form;
     });
 
-    if (response.statusCode != 302 ||
-        !response.headers.location.includes(`/addon/manage/${this.addonSlug}/`)) {
-      throw new Error("Updating failed: " + response.statusCode);
+    let responses = forms.map((form, page0) => {
+      let uri = `${AMO_ADMIN_BASE}/addon/manage/${this.addonSlug}/?page=${page0 + 1}`;
+      return this.session.request({
+        method: "POST",
+        uri: uri,
+        form: form,
+        headers: { Referer: uri }
+      });
+    });
+
+    let results = await Promise.allSettled(responses);
+    let failed = results.reduce((acc, { status, value: response, reason }, page0) => {
+      if (status != "fulfilled" ||
+          response.statusCode != 302 ||
+          !response.headers.location.includes(`/addon/manage/${this.addonSlug}/`)) {
+        acc.push(`page ${page0 + 1}: ${reason || response.statusCode}`);
+      }
+
+      return acc;
+    });
+
+    if (failed.length) {
+      throw new Error("Updating failed: " + errors.join(","));
     }
   }
 
-  async disableFiles(files=null) {
+  async disableAllFiles() {
     await this.ensureLoaded();
 
-    if (!files) {
-      files = this.files;
-    }
-
-    for (let file of files) {
+    for (let file of this.files) {
       if (file.status !== ADDON_STATUS.DELETED) {
         file.status = ADDON_STATUS.DISABLED;
       }
@@ -178,14 +197,10 @@ export class AddonAdminPage {
     }
   }
 
-  async enableFiles(files=null) {
+  async enableAllFiles() {
     await this.ensureLoaded();
 
-    if (!files) {
-      files = this.files;
-    }
-
-    for (let file of files) {
+    for (let file of this.files) {
       file.status = ADDON_STATUS.APPROVED;
     }
 
@@ -196,14 +211,28 @@ export class AddonAdminPage {
 
   async enableVersions(versions) {
     let versionSet = new Set(versions);
-    let toEnable = this.files.filter(file => versionSet.has(file.version.name));
-    return this.enableFiles(toEnable);
+    for (let file of this.files) {
+      if (versionSet.has(file.version.name) && file.status != ADDON_STATUS.DELETED) {
+        file.status = ADDON_STATUS.APPROVED;
+      }
+    }
+
+    if (this.autocommit) {
+      await this.update();
+    }
   }
 
   async disableVersions(versions) {
     let versionSet = new Set(versions);
-    let toEnable = this.files.filter(file => versionSet.has(file.version.name));
-    return this.disableFiles(toEnable);
+    for (let file of this.files) {
+      if (versionSet.has(file.version.name)) {
+        file.status = ADDON_STATUS.DISABLED;
+      }
+    }
+
+    if (this.autocommit) {
+      await this.update();
+    }
   }
 }
 
